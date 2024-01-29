cc605c6ca4b1dd26e669846a472b20ffe0345aef
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-01-29 11:40:13 +0100

    Show favorite contacts

--- controllers/contacts/createContact.js
+++ controllers/contacts/createContact.js
@@ -1,11 +1,10 @@
-import { addNewContact, findUser } from '#helpers/helpers.js';
+import { addNewContact } from '#helpers/helpers.js';
 
 export async function createContact(req, res, next) {
   try {
     const user = res.user;
     const { name, email } = req.body;
     const newContact = await addNewContact({ name, email, owner: user._id });
-    console.log(newContact);
     newContact ? res.status(201).json(newContact) : next();
   } catch (error) {
     next(error);
--- controllers/contacts/indexContacts.js
+++ controllers/contacts/indexContacts.js
@@ -1,10 +1,15 @@
-import { getAllContacts } from '#helpers/helpers.js';
+import { getAllContacts, findFavoriteContacts } from '#helpers/helpers.js';
 
 export async function indexContacts(req, res, next) {
   try {
     const user = res.user;
-    const contacts = await getAllContacts(user);
-    return res.status(200).json(contacts);
+    const params = req.query;
+    if (params.favorite) {
+      const favoriteContacts = await findFavoriteContacts(user);
+      return res.status(200).json(favoriteContacts);
+    }
+    const allContacts = await getAllContacts(user);
+    return res.status(200).json(allContacts);
   } catch (error) {
     next(error);
   }
