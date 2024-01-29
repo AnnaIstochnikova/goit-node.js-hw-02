cc872fff471a4dd054ed7fa39248701e2914a421
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-01-29 01:03:31 +0100

    Get current user function

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -6,7 +6,7 @@ const findAndDeleteContact = id => Contact.findByIdAndDelete(id);
 const addNewContact = contact => Contact.create(contact);
 const findContact = filter => Contact.findOne(filter);
 const findAndUpdateContact = (filter, updateField) =>
-  Contact.findOneAndUpdate(filter, { $set: updateField }, { new: true });
+  Contact.findOneAndUpdate(filter, updateField, { new: true });
 
 const findUser = filter => User.findOne(filter);
 const findAndUpdateUser = (filter, updateField) =>
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,15 @@ import express from 'express';
 
 import { login } from '#controllers/users/login.js';
 import { signup } from '#controllers/users/signup.js';
-import { authMiddleware } from '#middleware/authMiddleware.js';
 import { logout } from '#controllers/users/logout.js';
+import { authMiddleware } from '#middleware/authMiddleware.js';
+import { getCurrentUserData } from '#controllers/users/getCurrentUserData.js';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/signup', signup);
 usersRouter.post('/login', login);
 usersRouter.get('/logout', authMiddleware, logout);
+usersRouter.get('/current', authMiddleware, getCurrentUserData);
 
 export { usersRouter };
