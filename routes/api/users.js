497dcf28ac180e764912997404aad1f9618b6b33
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-02-07 11:39:53 +0100

    Verify token endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,10 +3,11 @@ import express from 'express';
 import { login } from '#controllers/users/login.js';
 import { signup } from '#controllers/users/signup.js';
 import { logout } from '#controllers/users/logout.js';
-import { authMiddleware } from '#middleware/authMiddleware.js';
-import { getCurrentUserData } from '#controllers/users/getCurrentUserData.js';
-import { uploadAvatar } from '#controllers/users/uploadAvatar.js';
 import { upload } from '#middleware/avatarsMiddleware.js';
+import { authMiddleware } from '#middleware/authMiddleware.js';
+import { verifyToken } from '#controllers/users/verifyToken.js';
+import { uploadAvatar } from '#controllers/users/uploadAvatar.js';
+import { getCurrentUserData } from '#controllers/users/getCurrentUserData.js';
 
 const usersRouter = express.Router();
 
@@ -15,5 +16,6 @@ usersRouter.post('/login', login);
 usersRouter.get('/logout', authMiddleware, logout);
 usersRouter.get('/current', authMiddleware, getCurrentUserData);
 usersRouter.post('/avatars', authMiddleware, upload.single('avatar'), uploadAvatar);
+usersRouter.get('/verify/:verificationToken', verifyToken);
 
 export { usersRouter };
